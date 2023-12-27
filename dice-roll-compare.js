f1144e05a50608de584f41bf4160a1b85aabe832
wfhopkins <will.hop@gmail.com>
2023-12-26 23:02:40 -0800

    can add and remove players and HTML updates. roll function does not work rn

--- dice-roll-compare.js
+++ dice-roll-compare.js
@@ -15,48 +15,48 @@ window.onload = function() {
   }
 
   const playersInput = document.querySelector("#players");
+  const playerListContainer = document.querySelector("#player-list");
 
+  // Find and save the current value input by the user as the Number of Players 
   playersInput.addEventListener('input', function() {
     const numOfPlayers = parseInt(playersInput.value);
-    console.log(numOfPlayers);
+    console.log("Number of Players:", numOfPlayers);
+    
+  // Create a list from the players and add them to the HTML
+    const players = createPlayerList(numOfPlayers);
+    insertPlayerListIntoHTML(players);
   });
 
-  // const playersInput = () => {
-  //   const numOfPlayers = document.getElementById("players").value;
-  //   console.log("# of Players: ", numOfPlayers)
-  // }
+  // Function to create players equal to the input value
+  const createPlayerList = (numOfPlayers) => {
+    const players = [];
+    for (let i = 1; i <= numOfPlayers; i++) {
+      players.push(`Player ${i}`)
+    }
+    return players;
+  }
 
-  // playersInput();
-  // console.log("Players Input: ", playersInput());
+  const insertPlayerListIntoHTML = (players) => {
+    playerListContainer.innerHTML = "";
 
+
+    players.forEach(player => {
+      const playerRollResult = document.createElement("p");
+      playerRollResult.textContent = player;
+      playerListContainer.appendChild(playerRollResult);
+    });
 
   // Compare each player's die roll and return result
-  const compareRolls = (numOfSides, numOfPlayers) => {
-
-    const player1 = rollD(numOfSides);
-      // console.log("P1:", player1);
-
-    const player2 = rollD(numOfSides);
-      // console.log("P2:", player2);
-
-    // Determine which player rolled a higher number
-    const findWinner = () => {
-      if (player1 === player2) {
-        return "Tie!";
-      } else if (player1 > player2) {
-        return "Player 1 Wins";
-      } else {
-        return "Player 2 Wins";
-      }
-    }
-    findWinner();
-    // console.log(findWinner());
+  const compareRolls = (numOfSides, players) => {
+    players.forEach(player => {
+      const playerDiceRoll = rollD(numOfSides);
+      console.log(`${player} rolled: ${playerDiceRoll}`);
+      document.getElementById("#player-list").innerHTML = `Player ${players[i]} Roll: ${playerDiceRoll}`;
+    })
+  };
 
     // Update HTML to reflect results of die roll
     document.getElementById("which-die").innerHTML = `D${numOfSides}`;
-    document.getElementById("P1Roll").innerHTML = `Player 1 Roll: ${player1}`;
-    document.getElementById("P2Roll").innerHTML = `Player 2 Roll: ${player2}`;
-    document.getElementById("Winner").innerHTML = `${findWinner()}`;
   }
 
   // Add onclick listener to each button and
@@ -71,8 +71,33 @@ window.onload = function() {
     numOfSides = parseInt(this.dataset.sides);
     // console.log("buttonId", buttonId)
     // console.log("numOfSides", numOfSides)
-    compareRolls(numOfSides);
+    compareRolls(numOfSides, players);
     })
   });
-
 };
+
+
+  // OG Code for two player game only
+    // const player1 = rollD(numOfSides);
+    //   // console.log("P1:", player1);
+
+    // const player2 = rollD(numOfSides);
+    //   // console.log("P2:", player2);
+
+    // Determine which player rolled a higher number
+    // const findWinner = () => {
+    //   if (player1 === player2) {
+    //     return "Tie!";
+    //   } else if (player1 > player2) {
+    //     return "Player 1 Wins";
+    //   } else {
+    //     return "Player 2 Wins";
+    //   }
+    // }
+    // findWinner();
+    // console.log(findWinner());
+
+
+    // document.getElementById("P1Roll").innerHTML = `Player 1 Roll: ${player1}`;
+    // document.getElementById("P2Roll").innerHTML = `Player 2 Roll: ${player2}`;
+    // document.getElementById("Winner").innerHTML = `${findWinner()}`;
